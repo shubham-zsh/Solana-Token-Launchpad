bbe9bac6b3f73311946a9d640db815d6552207bc
Shubham Borse <shubhamborse0911@gmail.com>
2025-10-10 12:43:34 +0530

    setting up for create mint funtion

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,12 +1,25 @@
 
 export function TokenLaunchpad() {
-    return <div>
-        <input type="text" placeholder="Token Name" class="w-110 p-4 border mt-20 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
-        <input type="text" placeholder="Symbol" class="w-110 p-4 border mt-10 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
-        <input type="text" placeholder="Image Url" class="w-110 p-4 border mt-10 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
-        <input type="text" placeholder="Token Name" class="w-110 p-4 border mt-10 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
 
-        <button type="button" className="bg-green-500 mt-10 px-10 py-3 text-black-bold font-semibold font-sans">Create</button>
+    function createToken() {
+        
+        const name = document.getElementById("name").value;
+        const symbol = document.getElementById("symbol").value;
+        const imageUrl = document.getElementById("imageUrl").value;
+        const initialSupply = document.getElementById("initialSupply").value;
+
+    }
+
+    return <div>
+        <input id="name" type="text" placeholder="Token Name" class="w-110 p-4 border mt-20 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
+
+        <input id="symbol" type="text" placeholder="Symbol" class="w-110 p-4 border mt-10 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
+
+        <input id="imageUrl" type="text" placeholder="Image Url" class="w-110 p-4 border mt-10 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
+
+        <input id="initialSupply" type="text" placeholder="Initial supply" class="w-110 p-4 border mt-10 placeholder-gray-700 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"></input><br />
+
+        <button onClick={createToken} type="button" className="bg-green-500 mt-10 px-10 py-3 text-black-bold font-semibold font-sans">Create</button>
 
     </div>
 }
